0cfcd50af4106580a45241379d45583fd7e89efc
JengoWiegerinck <jengo.wiegerinck@student.graafschapcollege.nl>
2023-11-08 10:43:57 +0100

    Add / first commit

---
+++ config/databse.php
@@ -0,0 +1,9 @@
+<!--Hier maak ik constanten aan de ik weer kan gebruiken in het bestand Database Functions.php-->
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_NAME", "root");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+
+?>
