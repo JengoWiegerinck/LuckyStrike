c0e60e46ceacc8c995e8565915ca13b36371e4a1
LuukBosmann <Luuk.Bosmann@student.graafschapcollege.nl>
2023-11-09 10:26:03 +0100

    mappenstructuur aangepast

--- public/footer.php
+++ public/footer.php
@@ -1,6 +1,3 @@
-<?php
-        require_once("../layouts/default.php")
-    ?>
 <footer id="website-footer" class="w-full bg-secondary absolute bottom-0">
 <div class="bg-yellowKleur">
   <div class="container mx-auto text-blackKleur flex justify-between max-w-screen-xl font-light">
