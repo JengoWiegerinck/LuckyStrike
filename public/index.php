f1bc992d087837cac403fe29adf5c941c7d11b5c
LuukBosmann <Luuk.Bosmann@student.graafschapcollege.nl>
2023-11-24 13:28:48 +0100

    Public map cleaned

--- public/index.php
+++ public/index.php
@@ -1,50 +1,51 @@
+<!-- Author: Luuk -->
 <?php require_once 'header.php'; ?>
+
 <!-- Link Swiper's CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 
 <div class="font-sans w-full pb-20 md:py-20 min-h-[100vh]">
-
-<!-- Main Section on the Left -->
-<section class="grid grid-cols-1 md:grid-cols-2 gap-8 p-4">
-
-    <div class="w-full h-auto md:w-3/4 text-center my-16 pb-8 border border-gray-300 rounded-lg shadow-lg mx-auto md:ml-20">
-        <div class="swiper mySwiper h-3/5 w-full">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide h-full w-full">
-                <img class="object-cover w-full h-full rounded-lg" src="../assets/img/slider_01.jpg" alt="Slide 1">
+    <!-- Main Section on the Left -->
+    <section class="grid grid-cols-1 md:grid-cols-2 gap-8 p-4">
+        <div class="w-full h-auto md:w-3/4 text-center my-16 pb-8 border border-gray-300 rounded-lg shadow-lg mx-auto md:ml-20">
+            <div class="swiper mySwiper h-3/5 w-full">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide h-full w-full">
+                        <img class="object-cover w-full h-full rounded-lg" src="../assets/img/slider_01.jpg" alt="Slide 1">
+                    </div>
+                    <div class="swiper-slide h-full w-full">
+                        <img class="object-cover w-full h-full rounded-lg" src="../assets/img/slider_02.jpg" alt="Slide 2">
+                    </div>
+                    <div class="swiper-slide h-full w-full">
+                        <img class="object-cover w-full h-full rounded-lg" src="../assets/img/slider_03.jpg" alt="Slide 3">
+                    </div>
+                    <div class="swiper-slide h-full w-full">
+                        <img class="object-cover w-full h-full rounded-lg" src="../assets/img/slider_04.jpg" alt="Slide 4">
+                    </div>
+                </div>
+                <div class="swiper-pagination"></div>
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
             </div>
-            <div class="swiper-slide h-full w-full">
-                <img class="object-cover w-full h-full rounded-lg" src="../assets/img/slider_02.jpg" alt="Slide 2">
-            </div>
-            <div class="swiper-slide h-full w-full">
-                <img class="object-cover w-full h-full rounded-lg" src="../assets/img/slider_03.jpg" alt="Slide 3">
-            </div>
-            <div class="swiper-slide h-full w-full">
-                <img class="object-cover w-full h-full rounded-lg" src="../assets/img/slider_04.jpg" alt="Slide 4">
+            <div class="px-4">
+                <h1 class="text-lg md:text-4xl font-bold my-8">Prijzen</h1>
+                <p class="text-sm truncate md:text-md">Maandag t/m donderdag: € 24,00 per baan per uur</p>
+                <p class="text-sm truncate md:text-md">Vrijdag t/m zondag tot 18:00: € 28,00 per baan per uur</p>
+                <p class="text-sm truncate md:text-md">Vrijdag t/m zondag vanaf 18:00: € 33,50 per baan per uur</p>
             </div>
         </div>
-        <div class="swiper-pagination"></div>
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
-        </div>
-        <div class="px-4">
-        <h1 class="text-lg md:text-4xl font-bold my-8">Prijzen</h1>
-        <p class="text-sm truncate md:text-md">Maandag t/m donderdag: € 24,00 per baan per uur</p>
-        <p class="text-sm truncate md:text-md">Vrijdag t/m zondag tot 18:00: € 28,00 per baan per uur</p>
-        <p class="text-sm truncate md:text-md">Vrijdag t/m zondag vanaf 18:00: € 33,50 per baan per uur</p>
-        </div>
-    </div>
 
-    <div class="container w-3/4 mx-auto text-center md:flex md:flex-col md:items-center md:justify-center">
-    <div class="md:pb-20"> <!-- Add padding to the bottom on larger screens to create space for the footer -->
-        <h2 class="text-4xl font-semibold text-gray-800">Welkom bij Lucky Strike Bowling!</h2>
-        <p class="text-lg text-gray-600 mt-4">Ervaar de opwinding van het omverwerpen van kegels en geniet van een geweldige tijd met vrienden en familie.</p>
-        <a href="reservation.php" class="inline-block mt-8 px-6 py-3 bg-yellowKleur text-whiteKleur font-bold rounded">Maak een reservering</a>
-    </div>
-</div>
-</section>
+        <!-- Right Section with CTA -->
+        <div class="container w-3/4 mx-auto text-center md:flex md:flex-col md:items-center md:justify-center">
+            <div class="md:pb-20">
+                <h2 class="text-4xl font-semibold text-gray-800">Welkom bij Lucky Strike Bowling!</h2>
+                <p class="text-lg text-gray-600 mt-4">Ervaar de opwinding van het omverwerpen van kegels en geniet van een geweldige tijd met vrienden en familie.</p>
+                <a href="reservation.php" class="inline-block mt-8 px-6 py-3 bg-yellowKleur text-whiteKleur font-bold rounded">Maak een reservering</a>
+            </div>
+        </div>
+    </section>
 
-    
+
 </div>
 <?php require_once 'footer.php'; ?>
 
@@ -53,17 +54,16 @@
 
 <!-- Initialize Swiper -->
 <script>
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    loop: true,
-    speed: 1000,
-    pagination: {
-        el: ".swiper-pagination",
-      },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-  });
-
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        loop: true,
+        speed: 1000,
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 </script>
--- public/reservationConfirm.php
+++ public/reservationConfirm.php
@@ -1,48 +1,32 @@
+<!-- Author: Luuk -->
 <?php
-include 'header.php';
-require_once("../source/useful_functions.php");
-require_once("../source/db_user.php");
-require_once("../source/db_lane.php");
-require_once("../source/db_reservation.php");
+include_once 'header.php';
+require_once "../source/useful_functions.php";
+require_once "../source/db_user.php";
+require_once "../source/db_lane.php";
+require_once "../source/db_reservation.php";
 
-// Check if the user is logged in
 if (isset($_COOKIE['CurrUser'])) {
     $user = new user(getUserById($_COOKIE['CurrUser']));
 
-
-
-
-
-    // Check if the reservation form was submitted
     if (isset($_POST["reservation"])) {
-        // Retrieve reservation details from the form
-        $username = $_POST["username"];
-        $email = $_POST["email"];
-        $date = $_POST["date"];
-        $startTime = $_POST["startTime"];
-        $volwassen = $_POST["volwassen"];
-        $kinderen = $_POST["kinderen"];
+        $username   = $_POST["username"];
+        $email      = $_POST["email"];
+        $date       = $_POST["date"];
+        $startTime  = $_POST["startTime"];
+        $volwassen  = $_POST["volwassen"];
+        $kinderen   = $_POST["kinderen"];
 
         if (isset($_POST["urenBowlen"])) {
             $urenBowlen = $_POST["urenBowlen"];
         }
-
-
-
-        // Perform any additional processing or validation as needed
-        // ...
-
-        // Display the reservation confirmation and lane selection form
 ?>
-
         <!DOCTYPE html>
         <html lang="en">
 
         <head>
-            <!-- Include any necessary scripts or stylesheets -->
             <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-            <!-- Additional scripts or stylesheets can be added here -->
-            <title>Reservation Confirmation</title>
+            <title>Lucky Strike</title>
         </head>
 
         <body>
@@ -57,7 +41,6 @@ if (isset($_COOKIE['CurrUser'])) {
                                     <p class="mt-20 text-red-600 font-bold">! Alleen de volgende banen hebben beschikbare hulpmiddelen: </p>
                                     <p>
                                         <?php
-                                        // get all lanes where gates is 1
                                         $lanes = getAllLane();
                                         while ($lane = $lanes->fetch_assoc()) {
                                             if ($lane['gates'] == 1) {
@@ -69,7 +52,6 @@ if (isset($_COOKIE['CurrUser'])) {
                                 </div>
 
                                 <div class="lg:col-span-2">
-                                    <!-- Display information received from the form -->
                                     <p><strong>Naam:</strong> <?php echo $username; ?></p>
                                     <p><strong>Email:</strong> <?php echo $email; ?></p>
                                     <p><strong>Datum:</strong> <?php echo formatedatumNl($date); ?></p>
@@ -81,7 +63,6 @@ if (isset($_COOKIE['CurrUser'])) {
                                     <hr><br>
 
                                     <!-- Lane selection form goes here -->
-                                    <!-- You can use a dropdown, radio buttons, or any other input method for lane selection -->
                                     <form method="post" action="processReservation.php">
                                         <label for="lane">Selecteer maximaal twee banen:</label>
                                         <table class="w-full border-collapse">
@@ -98,7 +79,6 @@ if (isset($_COOKIE['CurrUser'])) {
                                                 <th class="text-center font-semibold text-gray-700 border-l-2 border-gray-300 p-2 lane-header cursor-pointer" id="lane8">Baan 8</th>
                                             </tr>
 
-                                            <!-- Existing code for the timeslots row -->
                                             <tr class="border-b-2 border-gray-300">
                                                 <td class="border-r-2 border-gray-300 p-2"><?php echo date('H:i', strtotime($startTime)); ?></td>
                                                 <?php
@@ -117,10 +97,7 @@ if (isset($_COOKIE['CurrUser'])) {
                                                             ?>
                                                             <?php if ($bool) { ?>
                                                                 <script>
-                                                                    // make the lane$i unclickable
                                                                     $("#lane<?php echo $i; ?>").removeClass("lane-header");
-
-                                                                    // add a visual indicator that the lane is unavailable by changing the opacity
                                                                     $(".lane-cell:nth-child(<?php echo $i + 1; ?>)").css("opacity", "0.5");
                                                                 </script>
                                                             <?php } ?>
@@ -150,10 +127,7 @@ if (isset($_COOKIE['CurrUser'])) {
                                                                 ?>
                                                                 <?php if ($bool) { ?>
                                                                     <script>
-                                                                        // make the lane$i unclickable
                                                                         $("#lane<?php echo $i; ?>").removeClass("lane-header");
-
-                                                                        // add a visual indicator that the lane is unavailable by changing the opacity
                                                                         $(".lane-cell:nth-child(<?php echo $i + 1; ?>)").css("opacity", "0.5");
                                                                     </script>
                                                                 <?php } ?>
@@ -172,9 +146,8 @@ if (isset($_COOKIE['CurrUser'])) {
                                         <input type="hidden" name="volwassen" value="<?php echo htmlspecialchars($volwassen); ?>">
                                         <input type="hidden" name="kinderen" value="<?php echo htmlspecialchars($kinderen); ?>">
                                         <input type="hidden" name="urenBowlen" value="<?php echo isset($urenBowlen) ? htmlspecialchars($urenBowlen) : ''; ?>">
-                                        <!-- Add hidden input field for selected lanes -->
                                         <input type="hidden" name="selectedLanes" id="selectedLanes" value="">
-                                        <!-- back button -->
+
                                         <a href="reservation.php" class="bg-yellowKleur hover:bg-blackKleur text-white font-bold my-6 py-2 px-4 rounded">Terug</a>
                                         <button type="submit" id="confirmationButton" class="bg-yellowKleur hover:bg-blackKleur text-white font-bold my-6 py-2 px-4 rounded">Bevestigen</button>
                                     </form>
@@ -189,38 +162,28 @@ if (isset($_COOKIE['CurrUser'])) {
                 var lanes = [];
 
                 $(document).ready(function() {
-                    // Add click event to each lane link in th elements
                     $(".lane-header").on("click", function() {
                         var laneIndex = $(this).index() - 1; // Subtract 1 to account for the date column
-                        // if a lane is already selected and the user clicks on it again, remove the background color
+
                         if (lanes.includes(laneIndex)) {
                             $(".lane-cell:nth-child(" + (laneIndex + 2) + ")").css("background-color", "");
-                            // Remove the lane from the array
                             lanes.splice(lanes.indexOf(laneIndex), 1);
                             return;
                         }
 
                         if (lanes.length == 2) {
-                            // If two lanes are already selected, remove the background color from the cells in the first lane
                             $(".lane-cell:nth-child(" + (lanes[0] + 2) + ")").css("background-color", "");
-                            // Remove the first lane from the array
                             lanes.shift();
                         }
-                        // Remove background color from all lanes
-                        // $(".lane-cell").css("background-color", "");
 
-                        // Get the index of the clicked lane header
                         lanes.push(laneIndex);
-                        // Set background color for the corresponding cells in the clicked lane
                         $(".lane-cell:nth-child(" + (laneIndex + 2) + ")").css("background-color", "#f0f0f0");
                     });
 
                     // Update the hidden input field with selected lanes and other form data
                     $("#confirmationButton").on("click", function() {
-                        // Convert the lanes array to JSON and set the value of the selectedLanes hidden field
                         $("#selectedLanes").val(JSON.stringify(lanes));
 
-                        // Get other form data and set the values of the corresponding hidden fields
                         var username = "<?php echo htmlspecialchars($username); ?>";
                         var email = "<?php echo htmlspecialchars($email); ?>";
                         var date = "<?php echo htmlspecialchars($date); ?>";
@@ -239,20 +202,16 @@ if (isset($_COOKIE['CurrUser'])) {
                     });
                 });
             </script>
-
         </body>
 
         </html>
-
 <?php
     } else {
-        // Redirect the user to the reservation page if the form was not submitted
         header('location: reservation.php');
     }
 
-    include 'footer.php';
+    include_once 'footer.php';
 } else {
-    // Redirect the user to the login page if not logged in
     header('location: login.php');
 }
 ?>
